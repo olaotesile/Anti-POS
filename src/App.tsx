77ce97a013d6838ce67adcaf7777c9700be49a94
olaotesile <bootesile@gmail.com>
2026-02-03 13:23:00 -0800

    first push

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>AntipOS</h1>
+      <p>A goofy alternative to POS machines.</p>
+    </div>
+  )
+}
+
+export default App
